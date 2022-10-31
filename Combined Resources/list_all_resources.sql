c5ed4cd6ed079b3bed03965e378ecc16356c0fe9
richard <the.anykey@gmail.com>
2022-11-01 00:04:07 +0100

    added full compartment path query

--- Combined Resources/list_all_resources.sql
+++ Combined Resources/list_all_resources.sql
@@ -3,6 +3,7 @@ select
   display_name,
   lifecycle_state as status,
   'DB' as Service,
+  database_edition as shape,
   cpu_core_count as OCPUs,
   null as MemoryGB,
   tags['CreatedBy'] as owner,
@@ -16,6 +17,7 @@ select
  display_name,
  lifecycle_state as status,
  'Compute' as Service,
+ shape as shape,
  shape_config_ocpus as OCPUs,
  shape_config_memory_in_gbs as MemoryGB,
  tags['CreatedBy'] as owner,
@@ -23,7 +25,19 @@ select
  _ctx['connection_name'] as tenancy
 from oci_core_instance
 
+union
 
+select
+ display_name,
+ lifecycle_state as status,
+ 'AutonomousDB' as Service,
+ db_workload || ' - ' ||  db_version as shape,
+ cpu_core_count as OCPUs,
+ null as MemoryGB,
+ tags['CreatedBy'] as owner,
+  compartment_id,
+ _ctx['connection_name'] as tenancy
+from oci_database_autonomous_database
 
 )
 
@@ -31,6 +45,7 @@ select
   Service,
   status,
   display_name as name,
+  shape,
   OCPUs,
   MemoryGB,
   owner,
